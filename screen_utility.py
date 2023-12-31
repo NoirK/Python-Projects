1c5822033ca6c5eba029dd34b32b5332f623a409
= <=>
2023-12-30 19:12:10 -0500

    Adding Python Script

---
+++ screen_utility.py
@@ -0,0 +1,12 @@
+class Screen_Position:
+    def __init__(self, x, y):
+        # the position of the screen
+        self.position = (x, y)
+
+    def get_pos(self):
+        # current position in memory
+        return self.position
+    
+    def update_pos(self,x ,y)
+        # updates the position
+        self.position = (x, y)
